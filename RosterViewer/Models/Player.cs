a18cf285752858931e4c1ad1cf6a48edca3e64da
tyn53 <jbush53@gmail.com>
2017-04-06 23:27:52 -0500

    Add Models

---
+++ RosterViewer/Models/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RosterViewer.Models
+{
+    public class Player
+    {
+        private int playerId;
+        private string playerScreenName;
+        private string playerFirstName;
+        private string playerLastname;
+        private Image playerAvatar;
+        private string currentTeam;
+        private List<Stat> playerStats;
+
+        public int PlayerId { get; set; }
+        public string PlayerScreenName { get; set; }
+        public string PlayerFirstName { get; set; }
+        public string PlayerLastName { get; set; }
+        public Image PlayerAvatar { get; set; }
+        public string CurrentTeam { get; set; }
+        public List<Stat> PlayerStats { get; set; }
+
+        
+    }
+}
---
+++ RosterViewer/Models/Stat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RosterViewer.Models
+{
+    public class Stat
+    {
+        private int statId;
+        private string statName;
+        private string statValue;
+
+        public int StatId { get; set; }
+        public string StatName { get; set; }
+        public string StatValue { get; set; }
+    }
+}
